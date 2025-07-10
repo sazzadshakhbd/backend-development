6e81dc19e724e92f212139533740fa94e6e799ba
sazzadshakhbd <sazzadshakhbd@gmail.com>
2025-07-10 07:44:24 +0600

    How to use forEach

---
+++ main.js
@@ -0,0 +1,12 @@
+// Fundaments of Javascript
+// Arrays and Objects
+// Functions returns
+// Async is coding
+// foreach, map, filter, find, indexOf,
+
+// var arr = [1, "sazzad", true, null, undefined, { name: "sazzad"}, function(){}, [1, 2], 1.5, NaN];
+var arr = [1, 2, 3, 4];
+arr.forEach(function(val){
+    // console.log(val + 1)
+    console.log(val + " Hello World");
+})
