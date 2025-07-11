f6dabf7bd7b2a0bed855f5964502d586349570e4
sazzadshakhbd <sazzadshakhbd@gmail.com>
2025-07-11 08:23:33 +0600

    how to use filter method

--- main.js
+++ main.js
@@ -6,10 +6,11 @@
 
 // var arr = [1, "sazzad", true, null, undefined, { name: "sazzad"}, function(){}, [1, 2], 1.5, NaN];
 var arr = [1, 2, 3, 4];
-var ans = arr.map(function(val){
-    // return 13;
-    // return val;
-    return val + 1
-
+var ans = arr.filter(function(val){
+    if(val >= 2) {
+        return true
+    } else {
+        return false
+    }
 })
 console.log(ans)
