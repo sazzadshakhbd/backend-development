0156e940efbe185a8aa61772dd1b3db4284c43a8
sazzadshakhbd <sazzadshakhbd@gmail.com>
2025-07-14 08:24:29 +0600

    how to apply object

--- main.js
+++ main.js
@@ -5,6 +5,23 @@
 // foreach, map, filter, find, indexOf,
 
 // var arr = [1, "sazzad", true, null, undefined, { name: "sazzad"}, function(){}, [1, 2], 1.5, NaN];
-var arr = [1, 2, 3, 4];
-var ans = arr.indexOf(3)
-console.log(ans)
+var obj = {
+    name: "sazzad",
+    age: 42,
+    isMarried: true,
+    address: {
+        city: "Dhaka",
+        country: "Bangladesh"
+    },
+    hobbies: ["coding", "reading", "gaming"],
+    greet: function() {
+        console.log("Hello, my name is " + this.name);
+    }
+}
+console.log(obj.name); // Accessing property
+console.log(obj.address.city); // Accessing nested property
+obj.greet(); // Calling a method
+console.log(obj.hobbies[1]); // Accessing array within object
+Object.freeze(obj); // Freezing the object to prevent further modifications
+obj.age = 43; // Updating a property
+console.log(obj["age"]); // Accessing property using bracket notation
