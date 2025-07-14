461cf0b07809948fac2253b74606a21d5c8813c0
sazzadshakhbd <sazzadshakhbd@gmail.com>
2025-07-14 08:50:28 +0600

    how to return any value in function

--- main.js
+++ main.js
@@ -5,23 +5,9 @@
 // foreach, map, filter, find, indexOf,
 
 // var arr = [1, "sazzad", true, null, undefined, { name: "sazzad"}, function(){}, [1, 2], 1.5, NaN];
-var obj = {
-    name: "sazzad",
-    age: 42,
-    isMarried: true,
-    address: {
-        city: "Dhaka",
-        country: "Bangladesh"
-    },
-    hobbies: ["coding", "reading", "gaming"],
-    greet: function() {
-        console.log("Hello, my name is " + this.name);
-    }
+
+function getValue() {
+    return  7
 }
-console.log(obj.name); // Accessing property
-console.log(obj.address.city); // Accessing nested property
-obj.greet(); // Calling a method
-console.log(obj.hobbies[1]); // Accessing array within object
-Object.freeze(obj); // Freezing the object to prevent further modifications
-obj.age = 43; // Updating a property
-console.log(obj["age"]); // Accessing property using bracket notation
+console.log(getValue()); // 7
+
